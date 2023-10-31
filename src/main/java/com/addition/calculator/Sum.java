e5782334918e9d31c9272faf057195468fa518fc
Rahulstar177 <rathodsachiit@gmaiil.com>
2023-10-31 18:32:20 +0530

    This is the demo

--- src/main/java/com/addition/calculator/Sum.java
+++ src/main/java/com/addition/calculator/Sum.java
@@ -2,85 +2,84 @@ package com.addition.calculator;
 
 public class Sum {
 
-	
-	
-	public int sum(int a , int b)
-	{
-		
-		int c= a+b;
-		System.out.println("Addtion Service: of a and b"+c);
-		
-		return c;
-	}
-	
-	public int sub(int a , int b)
-	{
-		
-		int c= a+b;
-		System.out.println("Addtion Service: of a and b"+c);
-		
+	public int sum(int a, int b) {
+
+		int c = a + b;
+		System.out.println("Addtion Service: of a and b" + c);
+
 		return c;
 	}
 
-	public int Test(int a , int b)
-	{
-		
-		int c= a+b;
-		System.out.println("Addtion Service: of a and b"+c);
-		
+	public int sub(int a, int b) {
+
+		int c = a + b;
+		System.out.println("Addtion Service: of a and b" + c);
+
 		return c;
 	}
 
+	public int Test(int a, int b) {
 
-		
-	public int Test1(int a , int b)
-	{
-		
-		int c= a+b;
-		System.out.println("Addtion Service: of a and b"+c);
-		
+		int c = a + b;
+		System.out.println("Addtion Service: of a and b" + c);
+
 		return c;
 	}
 
-	public int Test2(int a , int b)
-	{
-		
-		int c= a+b;
-		System.out.println("Addtion Service: of a and b"+c);
-		
-		return c;
-	}
-	public int Test3(int a , int b)
-	{
-		
-		int c= a+b;
-		System.out.println("Addtion Service: of a and b"+c);
-		
-		return c;
-	}
-	public int Test4(int a , int b)
-	{
-		
-		int c= a+b;
-		System.out.println("Addtion Service: of a and b"+c);
-		
+	public int Test1(int a, int b) {
+
+		int c = a + b;
+		System.out.println("Addtion Service: of a and b" + c);
+
 		return c;
 	}
 
-	public int Test5(int a , int b)
-	{
-		
-		int c= a+b;
-		System.out.println("Addtion Service: of a and b"+c);
-		
+	public int Test2(int a, int b) {
+
+		int c = a + b;
+		System.out.println("Addtion Service: of a and b" + c);
+
 		return c;
 	}
 
-	
-	
-	
-	
-	
-	
-	
+	public int Test3(int a, int b) {
+
+		int c = a + b;
+		System.out.println("Addtion Service: of a and b" + c);
+
+		return c;
+	}
+
+	public int Test4(int a, int b) {
+
+		int c = a + b;
+		System.out.println("Addtion Service: of a and b" + c);
+
+		return c;
+	}
+
+	public int Test5(int a, int b) {
+
+		int c = a + b;
+		System.out.println("Addtion Service: of a and b" + c);
+
+		return c;
+	}
+
+	public int Test6(int a, int b) {
+
+		int c = a + b;
+		System.out.println("Addtion Service: of a and b" + c);
+
+		return c;
+	}
+
+	public int Test7(int a, int b) {
+
+		int c = a + b;
+		System.out.println("Addtion Service: of a and b" + c);
+
+		return c;
+	}
+
 }
