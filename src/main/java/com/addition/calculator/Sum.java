3c047d56aaf5408a839cc168a85f6ad2ad290d74
Rahulstar177 <rathodsachiit@gmaiil.com>
2023-10-31 17:57:52 +0530

    This is the Stash demo

---
+++ src/main/java/com/addition/calculator/Sum.java
@@ -0,0 +1,31 @@
+package com.addition.calculator;
+
+public class Sum {
+
+	
+	
+	public int sum(int a , int b)
+	{
+		
+		int c= a+b;
+		System.out.println("Addtion Service: of a and b"+c);
+		
+		return c;
+	}
+	
+	public int sub(int a , int b)
+	{
+		
+		int c= a+b;
+		System.out.println("Addtion Service: of a and b"+c);
+		
+		return c;
+	}
+
+
+	
+	
+	
+	
+	
+}
